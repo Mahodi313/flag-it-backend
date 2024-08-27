6b5bf1c06eb9467981ed47cd535728d22a416238
Mahodi313 <Mehdiali15@outlook.com>
2024-08-27 10:55:35 +0200

    db-models-creation

---
+++ flag-it-backend/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace flag_it_backend.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public List<ResultModel> Results { get; set; }
+    }
+}
---
+++ flag-it-backend/Models/CountryModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace flag_it_backend.Models
+{
+    public class CountryModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Continent { get; set; }
+        public string? Description { get; set; }
+        public string? FlagImage { get; set; }
+        public QuestionModel? Question { get; set; }
+    }
+}
