7303161c2fdd81def5dd915150048e05f12f5a4a
druzhee <mohannadsa54@gmail.com>
2024-08-28 14:03:45 +0200

    QuestionController Completed

--- flag-it-backend/Repositories/Interfaces/IQuestionRepository.cs
+++ flag-it-backend/Repositories/Interfaces/IQuestionRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace flag_it_backend.Repositories.Interfaces
+﻿using flag_it_backend.Models;
+
+namespace flag_it_backend.Repositories.Interfaces
 {
     public interface IQuestionRepository
     {
-
+        public Task <List<QuestionModel>> GetQuestionByDifficultiesAsync (string difficulty);
+        public Task<QuestionModel> GetByCountryIdAsync(int countryId);
     }
 }
