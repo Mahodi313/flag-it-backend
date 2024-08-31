5f26d323ce066f153f8fc9ad0c3049fd9ba69c52
Mahodi313 <MehdiAli15@outlook.com>
2024-08-31 16:52:18 +0200

    Unit-tests-generic-repo

--- flag-it-backend.UnitTests/RepositoryTests/GenericRepoTesting.cs
+++ flag-it-backend.UnitTests/RepositoryTests/GenericRepoTesting.cs
@@ -14,7 +14,7 @@ using System.Threading.Tasks;
 
 namespace flag_it_backend.UnitTests.RepositoryTests
 {
-    public class GenericRepoTesting
+    public class GenericRepoTesting : IDisposable
     {
         private ServiceProvider _serviceProvider;
         private AppDbContext _dbContext;
@@ -24,7 +24,6 @@ namespace flag_it_backend.UnitTests.RepositoryTests
         {
 
             // SETUP
-
             var services = new ServiceCollection();
 
             services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("TestDb"));
@@ -46,6 +45,7 @@ namespace flag_it_backend.UnitTests.RepositoryTests
             _dbContext.SaveChanges();
         }
 
+        // CREATE
         [Fact]
         public async Task CreateAsync_AddsEntityToDatabase()
         {
@@ -53,16 +53,68 @@ namespace flag_it_backend.UnitTests.RepositoryTests
 
             var result = await _repository.CreateAsync(newEntity);
 
-            Assert.Equal(2, await _dbContext.Set<CountryModel>().CountAsync()); // Checks if there are now 2 entities (OBS! 1 is already created in the constructor).
-            Assert.Equal("New Test Entity", result.Name); // Check that the name of the added entity is correct.
+            Assert.Equal(2, await _dbContext.Set<CountryModel>().CountAsync()); // Checks if there are now 2 entities (OBS! 1 is already created in the constructor)
+            Assert.Equal("New Test Entity", result.Name); // Check that the name of the added entity is correct
 
             var entityInDb = await _dbContext.Set<CountryModel>().FindAsync(newEntity.Id);
             Assert.NotNull(entityInDb); // Checks that the entity was actually saved in the db
         }
 
+        // READ BY ID
+        [Fact]
+        public async Task GetByIdAsync_ReturnsEntity()
+        {
+            var entity = await _repository.GetByIdAsync(1);
+
+            Assert.NotNull(entity);
+            Assert.Equal(1, entity.Id);
+            Assert.Equal("Test Entity", entity.Name);
+        }
+
+        // READ(GET ALL)
+        [Fact]
+        public async Task GetAllAsync_ReturnsAllEntities()
+        {
+            var entities = await _repository.GetAllAsync();
+
+            Assert.NotNull(entities);
+            Assert.Single(entities); // Checks if there is only one entity in the list, since we only have 1 pre created
+            Assert.Equal("Test Entity", entities.First().Name); // Checks if the first entity of the list is equal to the provided one
+        }
+
+        // DELETE
+        [Fact]
+        public async Task DeleteAsync_RemovesEntityFromDatabase()
+        {
+            var deletedEntity = await _repository.DeleteAsync(1);
+
+            Assert.Equal(1, deletedEntity.Id); // Verify the ID of the deleted entity
+
+            var entityInDb = await _dbContext.Set<CountryModel>().FindAsync(1);
+            Assert.Null(entityInDb); // Checks if entity was removed from the db
+        }
+
+        // UPDATE
+        [Fact]
+        public async Task UpdateAsync_UpdatesEntityInDatabase()
+        {
+            var entityToUpdate = await _repository.GetByIdAsync(1);
+            entityToUpdate.Name = "Updated Entity";
+
+            await _repository.UpdateAsync(entityToUpdate);
+
+            var updatedEntity = await _dbContext.Set<CountryModel>().FindAsync(1);
+            Assert.NotNull(updatedEntity); // Checks if entity still exists
+            Assert.Equal("Updated Entity", updatedEntity.Name); // Verifies that the name was updated
+        }
+
+
+
+
         // Cleans up the environment after each test
         public void Dispose()
         {
+            Console.WriteLine("Calling");
             _dbContext.Database.EnsureDeleted(); // Delete the in memory db
             _dbContext.Dispose(); // Dispose of the dbcontext
             _serviceProvider.Dispose(); // Dispose of the service provider
